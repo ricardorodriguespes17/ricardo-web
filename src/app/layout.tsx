f1997dc2b3ccdb86969612e29c2ba923515b65ce
Ricardo Rodrigues <ricardor662@gmail.com>
2024-12-07 18:25:44 -0300

    feat: define font family montserrat

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,24 @@
-import type { Metadata } from "next";
-import "./globals.css";
+import type { Metadata } from "next"
+import { Montserrat } from "next/font/google"
+import "./globals.css"
 
 export const metadata: Metadata = {
   title: "Ricardo Rodrigues | Dev. Fullstack",
   description: "Site de apresentação de Ricardo Rodrigues",
-};
+}
+
+const font = Montserrat({
+  subsets: ["latin"],
+})
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
-    <html lang="pt-br">
+    <html lang="pt-br" className={font.className}>
       <body>{children}</body>
     </html>
-  );
+  )
 }
