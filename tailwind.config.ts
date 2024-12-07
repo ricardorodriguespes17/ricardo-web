b1f9c77e28efc8d9b72a5633752d6a87c19dae2c
Ricardo Rodrigues <ricardor662@gmail.com>
2024-12-07 18:23:10 -0300

    feat: create colors pallette

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,6 +6,28 @@ export default {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  theme: {},
+  theme: {
+    extend: {
+      colors: {
+        green: {
+          dark: "#55e5a4",
+          light: "#61C698",
+        },
+        gray: {
+          10: "##f0f0f4",
+          20: "#EEEEEE",
+          30: "#E3E3E3",
+          50: "#A9A9A9",
+          80: "#686565",
+          90: "#77808B",
+          100: "#8C8C8C",
+        },
+        dark: {
+          90: "#26313F",
+          100: "#151C25",
+        },
+      },
+    },
+  },
   plugins: [],
 } satisfies Config
