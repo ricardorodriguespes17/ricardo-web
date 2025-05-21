6c1e755cf61bb364a47980f839d2a27552a79722
Ricardo Rodrigues <ricardor662@gmail.com>
2025-05-21 09:43:02 -0300

    add skills data

--- src/store/skillsStore.ts
+++ src/store/skillsStore.ts
@@ -11,11 +11,21 @@ type Actions = {
 
 const useSkills = create<State & Actions>()((set) => ({
   skills: [
-    { label: "React", logo: "react", type: "frontend" },
-    { label: "React", logo: "react", type: "frontend" },
-    { label: "React", logo: "react", type: "backend" },
-    { label: "React", logo: "react", type: "backend" },
-    { label: "React", logo: "react", type: "interpersonal" },
+    { label: "React JS", logo: "react", type: "frontend" },
+    { label: "Next JS", logo: "next", type: "frontend" },
+    { label: "Tailwind CSS", logo: "tailwind", type: "frontend" },
+    { label: "Angular JS", logo: "angular", type: "frontend" },
+    { label: "Zustand", logo: "zustand", type: "frontend" },
+    { label: "Node JS", logo: "node", type: "backend" },
+    { label: "Java", logo: "java", type: "backend" },
+    { label: "Prisma", logo: "prisma", type: "backend" },
+    { label: "Agentes de IA", logo: "ia", type: "backend" },
+    { label: "Firebase", logo: "firebase", type: "backend" },
+    { label: "Proatividade", type: "interpersonal" },
+    { label: "Confiança", type: "interpersonal" },
+    { label: "Criatividade", type: "interpersonal" },
+    { label: "Vesatilidade", type: "interpersonal" },
+    { label: "Facilidade de aprender", type: "interpersonal" },
   ],
   setSkills: (skills) => set({ skills }),
 }))
