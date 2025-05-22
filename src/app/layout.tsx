4fb11f83b9af912683435524d4970dbaca848e01
Ricardo Rodrigues <ricardor662@gmail.com>
2025-05-22 19:44:25 -0300

    feat: add favicon and another meta data

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,24 +1,39 @@
 import type { Metadata } from "next"
 import { Montserrat } from "next/font/google"
 import "./globals.css"
+import data from "@/data"
 
 export const metadata: Metadata = {
-  title: "Ricardo Rodrigues | Dev. Fullstack",
-  description: "Site de apresentação de Ricardo Rodrigues",
+  title: `${data.presentation.title} | ${data.presentation.subtitle}`,
+  description: `Site de apresentação de ${data.presentation.title}`,
+  openGraph: {
+    title: `${data.presentation.title} | ${data.presentation.subtitle}`,
+    description: `Site de apresentação de ${data.presentation.title}`,
+    type: "website",
+    images: [
+      {
+        url: data.portifolio.avatar,
+        width: 1200,
+        height: 630,
+        alt: "Texto alternativo da imagem",
+      },
+    ],
+  },
+  icons: {
+    icon: "/favicon.ico",
+  },
 }
 
 const font = Montserrat({
   subsets: ["latin"],
 })
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode
-}>) {
+function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="pt-br" className={font.className}>
       <body>{children}</body>
     </html>
   )
 }
+
+export default RootLayout
