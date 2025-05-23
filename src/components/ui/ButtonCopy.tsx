be60c73b124facc176196ac44c805996915aa27f
Ricardo Rodrigues <ricardor662@gmail.com>
2025-05-23 15:50:32 -0300

    feat: use Alert component to show copy message

--- src/components/ui/ButtonCopy.tsx
+++ src/components/ui/ButtonCopy.tsx
@@ -2,17 +2,22 @@
 
 import { FiCopy } from "react-icons/fi"
 import Button from "./Button"
+import useAlert from "@/store/alertStore"
 
 type Props = {
   text: string
 }
 
 const ButtonCopy = (props: Props) => {
+  const { showAlert } = useAlert()
+
   const handleCopy = () => {
     navigator.clipboard
       .writeText(props.text)
       .then(() => {
-        alert("Texto copiado para a área de transferência!")
+        showAlert({
+          content: "Texto copiado para a área de transferência!",
+        })
       })
       .catch((err) => {
         console.error("Falha ao copiar o texto: ", err)
