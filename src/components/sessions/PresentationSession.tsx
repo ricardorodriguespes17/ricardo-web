219d170edd31f28939a133ead2ef203155f65375
Ricardo Rodrigues <ricardor662@gmail.com>
2025-05-22 23:42:19 -0300

    improve: presentation session top space in mobile

--- src/components/sessions/PresentationSession.tsx
+++ src/components/sessions/PresentationSession.tsx
@@ -3,6 +3,7 @@ import ProfileImage from "../common/ProfileImage"
 import Point from "../common/Point"
 import Button from "../ui/Button"
 import data from "@/data"
+import { twMerge } from "tailwind-merge"
 
 const {
   title,
@@ -16,9 +17,14 @@ const {
 
 const PresentationSession = () => {
   return (
-    <div className="min-h-screen h-fit w-full px-8 sm:px-16 md:px-24 lg:px-32 flex items-center">
+    <div
+      className={twMerge(
+        "h-fit w-full px-8 flex min-h-screen items-center",
+        "sm:px-16 md:px-24 lg:px-32 md:mt-0"
+      )}
+    >
       <div className="flex flex-col-reverse md:flex-row items-center h-fit w-full">
-        <div className="flex flex-1 flex-col justify-center items-center md:items-start gap-3 md:gap-6">
+        <div className="flex flex-1 flex-col justify-center items-center md:items-start gap-6 md:gap-6">
           <div className="flex flex-col gap-1 items-center md:items-start">
             <h1 className="dark:text-blue-dark">{title}</h1>
             <p className="text-gray-80 dark:text-blue-light">{subtitle}</p>
@@ -41,7 +47,7 @@ const PresentationSession = () => {
 
           <Button
             href="#contato"
-            className="hover:bg-blue-light hover:text-dark-100"
+            className="hover:bg-blue-light hover:text-dark-100 mt-10 md:mt-5"
           >
             {contactButtonText}
           </Button>
