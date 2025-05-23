f15d0eb95ee6c2bac3718f6c389c4a433fa06501
Ricardo Rodrigues <ricardor662@gmail.com>
2025-05-22 23:45:49 -0300

    improve: drawer outside in dark theme

--- src/components/layout/Drawer/DrawerOutside.tsx
+++ src/components/layout/Drawer/DrawerOutside.tsx
@@ -9,8 +9,8 @@ const DrawerOutside = (props: Props) => {
   return (
     <div
       className={twMerge(
-        "flex-1 h-full bg-black/30 transition-all duration-300",
-        !props.isOpen && "bg-transparent"
+        "flex-1 h-full bg-black/30 dark:bg-blue-dark/10 transition-all duration-300",
+        !props.isOpen && "bg-transparent dark:bg-transparent"
       )}
       onClick={props.onClose}
     />
